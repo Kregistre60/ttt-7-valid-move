196ec07b957ab1f4f7b7f35bacaa29c03cbeb124
Kervens Registre <Kregistre60@gmail.com>
2022-01-01 12:30:06 -0500

    commit tree be64ea216db50f6041680d5a0e89e8e8923dbd1b

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,26 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+    if !position_taken?(board, index) && (index).between?(0,8)
+      return true
+    else 
+      return false
+    end
+  end
 
+# re-define your #index_taken? method here, so that you can use it in the #valid_move? method above.
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,position)
+    
+    if board[position] == " "
+      false
+      elsif board[position] == ""
+      false
+      elsif  board[position] == nil
+      false
+      else   board[position] == "X" || board[position] == "O"
+      true
+      return false if [" ", "", nil].include?(board[position])
+  return true if ["X", "O"].include?(board[position])
+  raise "#{board[position]} is not a valid move"
+    end
+  end
